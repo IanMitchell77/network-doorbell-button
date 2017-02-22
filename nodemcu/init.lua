37b55f7be9ddf5de8756a5512d7fcd16a5bc33c2
Ian Mitchell <imi06@localhost.localdomain>
2017-02-22 21:52:35 +0000

    It now sends push messages when the flash button is pressed, its debounced a little, the led comes on while is is connecting to wifi and off after, it flashes when the button is pressed too.

--- nodemcu/init.lua
+++ nodemcu/init.lua
@@ -1,25 +1,37 @@
+buttonpin=3
+ledpin=4
+
+gpio.mode(ledpin,gpio.OUTPUT)
+gpio.write(ledpin, gpio.LOW)
+
 wifi.setmode(wifi.STATION)
+
 -- in the house
 --wifi.sta.config("SKYB2595","XNTRRWRQYB")
 
 -- in the office
-wifi.sta.config("Mitchsoft","davethecat")
+--wifi.sta.config("Mitchsoft","davethecat")
+
+-- in Jules's house.    
+wifi.sta.config("VM6339503","zdhzmcFCy2vr")
 
 wifi.sta.connect()
+
+print("Connecting to Wifi...")
+
 tmr.alarm(1, 1000, 1, function()
     if wifi.sta.getip() == nil then
         print("IP unavaiable, Waiting...")
     else
         tmr.stop(1)
-        print("ESP8266 mode is: " .. wifi.getmode())
-        print("The module MAC address is: " .. wifi.ap.getmac())
-        print("Config done, IP is "..wifi.sta.getip())
-
-        print("The gpio is: " ..gpio.read(2))
+        gpio.write(ledpin, gpio.HIGH)
+        --print("ESP8266 mode is: " .. wifi.getmode())
+        --print("The module MAC address is: " .. wifi.ap.getmac())
+        print("Connected, my IP is "..wifi.sta.getip())
         
-
-        sendButtonPress()
-        
+        print("Now waiting for button presses on GPIO " .. buttonpin)
+        gpio.mode(buttonpin, gpio.INT, gpio.PULLUP) -- see https://github.com/hackhitchin/esp8266-co-uk/pull/1
+        gpio.trig(buttonpin, 'down', debounce(onChange))
     end
 end)
 
@@ -27,4 +39,26 @@ function sendButtonPress()
   print("Sending a button press!")
   local socket =  net.createUDPSocket() 
   socket:send( 4950, "192.168.0.255", "doorbell-button-press")
+end
+
+function debounce (func)
+    local last = 0
+    local delay = 1500000
+
+    return function (...)
+        local now = tmr.now()
+        local delta = now - last
+        if delta < 0 then delta = delta + 2147483647 end; -- proposed because of delta rolling over, https://github.com/hackhitchin/esp8266-co-uk/issues/2
+        if delta < delay then return end;
+
+        last = now
+        return func(...)
+    end
+end
+
+function onChange ()
+    --print('The pin value has changed')
+    gpio.write(ledpin, gpio.LOW)
+    sendButtonPress()
+    gpio.write(ledpin, gpio.HIGH)
 end
