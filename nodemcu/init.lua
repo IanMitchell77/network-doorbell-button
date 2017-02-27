c9e3a8115ba74d6168b6ed76f9842d1ab5e23ff8
Ian Mitchell <imi06@UNKNOWN.bskyb.com>
2017-02-27 15:56:53 +0000

    Use vairiable for the network crudentials, add comments and make the code nicer to read.

--- nodemcu/init.lua
+++ nodemcu/init.lua
@@ -1,44 +1,54 @@
+--define the gpio pins
+--the flash button on the dev board
 buttonpin=3
+--the led on the dev board
 ledpin=4
 
+--set wifi details
+-- in Jules's house. 
+--ssid="VM6339503"
+--key="zdhzmcFCy2vr"
+-- in the house
+--ssid="SKYB2595"
+--key="XNTRRWRQYB"
+-- in the office
+ssid="Mitchsoft"
+key="davethecat"
+
+--turn on the led
 gpio.mode(ledpin,gpio.OUTPUT)
 gpio.write(ledpin, gpio.LOW)
 
+--tell the wifi modlule that it is a station
 wifi.setmode(wifi.STATION)
 
--- in the house
---wifi.sta.config("SKYB2595","XNTRRWRQYB")
-
--- in the office
---wifi.sta.config("Mitchsoft","davethecat")
-
--- in Jules's house.    
-wifi.sta.config("VM6339503","zdhzmcFCy2vr")
-
+--connect to wifi
+wifi.sta.config(ssid,key)
 wifi.sta.connect()
-
-print("Connecting to Wifi...")
+print("Connecting to "..ssid)
 
 tmr.alarm(1, 1000, 1, function()
     if wifi.sta.getip() == nil then
         print("IP unavaiable, Waiting...")
     else
         tmr.stop(1)
+	--turn the led off
         gpio.write(ledpin, gpio.HIGH)
         --print("ESP8266 mode is: " .. wifi.getmode())
         --print("The module MAC address is: " .. wifi.ap.getmac())
         print("Connected, my IP is "..wifi.sta.getip())
-        
         print("Now waiting for button presses on GPIO " .. buttonpin)
+	--setup the button gpio to use the internal pullup
         gpio.mode(buttonpin, gpio.INT, gpio.PULLUP) -- see https://github.com/hackhitchin/esp8266-co-uk/pull/1
+	--setup and register the interrupt
         gpio.trig(buttonpin, 'down', debounce(onChange))
     end
 end)
 
 function sendButtonPress()
-  print("Sending a button press!")
-  local socket =  net.createUDPSocket() 
-  socket:send( 4950, "192.168.0.255", "doorbell-button-press")
+    print("Sending a button press!")
+    local socket =  net.createUDPSocket() 
+    socket:send( 4950, "192.168.0.255", "doorbell-button-press")
 end
 
 function debounce (func)
@@ -57,7 +67,7 @@ function debounce (func)
 end
 
 function onChange ()
-    --print('The pin value has changed')
+    --flash the led
     gpio.write(ledpin, gpio.LOW)
     sendButtonPress()
     gpio.write(ledpin, gpio.HIGH)
